4744f5702db62c559860e2bc3a557931b2133c9c
Nandha Kishore <jnandhakishore12@gmail.com>
2026-03-07 12:01:12 -0800

    Add image resources

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  publicDir: path.resolve(__dirname, 'public'),
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@the-green-felt/shared': path.resolve(__dirname, '../shared/src'),
+    },
+  },
   server: {
     port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 500,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
